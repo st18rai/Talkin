484df508a2db7e544fa903f354c0e330b867cb3f
Koroqe <koroqe@gmail.com>
2017-01-18 00:00:00 +0200

    Hiden label on existing activities

--- app/src/main/java/com/internship/droidz/talkin/SplashScreen.java
+++ app/src/main/java/com/internship/droidz/talkin/SplashScreen.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -13,6 +15,7 @@ public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_splash_screen);
 
         new Handler().postDelayed(new Runnable() {
