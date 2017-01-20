7d7e0d0bdf39e5e275387a551e9ea6bb746554f7
Alexandr Novak <joykerdp@gmail.com>
2017-01-20 14:14:44 +0200

    signup retrofit

--- app/src/main/java/com/internship/droidz/talkin/web/ApiManager.java
+++ app/src/main/java/com/internship/droidz/talkin/web/ApiManager.java
@@ -2,9 +2,11 @@ package com.internship.droidz.talkin.web;
 
 import com.google.gson.GsonBuilder;
 
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
+import rx.schedulers.Schedulers;
 
 /**
  * Created by Novak Alexandr on 19.01.2017.
@@ -19,24 +21,36 @@ public class ApiManager {
     private static final String APP_SECRET= "zbhG3xMwDPrWyzf";
     private static final String SRV=SCHEMA+HOST;
 
+    private static OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+    private static ApiService service;
 
-    private Retrofit retrofit;
-    private ApiService apiService;
-
-    public void init()
-    {
-        retrofit = new Retrofit.Builder()
-                .baseUrl(SRV)
-                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
-                .addConverterFactory(createGsonConverter())
-                .build();
-
-        apiService = retrofit.create(ApiService.class);
-    }
-
-    private GsonConverterFactory createGsonConverter() {
+    private  static GsonConverterFactory createGsonConverter() {
         GsonBuilder builder = new GsonBuilder();
         builder.serializeNulls();
         return GsonConverterFactory.create(builder.create());
     }
+
+    private static Retrofit.Builder builder =
+            new Retrofit.Builder()
+                    .baseUrl(SRV)
+                    .addConverterFactory(createGsonConverter())
+                    .addCallAdapterFactory(RxJavaCallAdapterFactory.createWithScheduler(Schedulers.io()));
+
+    public static <S> S createService(Class<S> serviceClass) {
+        Retrofit retrofit = builder.client(httpClient.build()).build();
+        return retrofit.create(serviceClass);
+    }
+
+    public static ApiService getService()
+    {
+        if(service==null){
+            return createService(ApiService.class);
+        }
+        else {
+            return service;
+        }
+
+
+    }
+
 }
