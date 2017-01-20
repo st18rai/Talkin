36631fc5ae932d8a53505eae04a2ef8f1c2d7ea7
st18rai <st18rai@gmail.com>
2017-01-20 18:35:27 +0200

    Created RegistrationScreen UI.

---
+++ app/src/main/java/com/internship/droidz/talkin/mvp/registration/RegistrationContract.java
@@ -0,0 +1,21 @@
+package com.internship.droidz.talkin.mvp.registration;
+
+/**
+ * Created by st18r on 20.01.2017.
+ */
+
+public interface RegistrationContract {
+
+    interface RegistrationModel {
+
+    }
+
+    interface RegistrationPresenter {
+
+    }
+
+
+    interface RegistrationView {
+
+    }
+}
---
+++ app/src/main/java/com/internship/droidz/talkin/mvp/registration/RegistrationScreen.java
@@ -0,0 +1,22 @@
+package com.internship.droidz.talkin.mvp.registration;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import com.internship.droidz.talkin.R;
+
+public class RegistrationScreen extends AppCompatActivity implements RegistrationContract.RegistrationView {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration_screen);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+    }
+
+}
