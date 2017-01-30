f7d7f5dd8c046b2de39c9abfbb32cbb574a5486a
Koroqe <koroqe@gmail.com>
2017-01-31 00:09:04 +0200

    Correct setting up user picture

--- app/src/main/java/com/internship/droidz/talkin/mvp/registration/RegistrationModel.java
+++ app/src/main/java/com/internship/droidz/talkin/mvp/registration/RegistrationModel.java
@@ -1,8 +1,23 @@
 package com.internship.droidz.talkin.mvp.registration;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.net.Uri;
+import android.os.Environment;
 
 import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import ru.tinkoff.decoro.MaskImpl;
+import ru.tinkoff.decoro.parser.UnderscoreDigitSlotsParser;
+import ru.tinkoff.decoro.slots.Slot;
+import ru.tinkoff.decoro.watchers.FormatWatcher;
+import ru.tinkoff.decoro.watchers.MaskFormatWatcher;
 
 /**
  * Created by st18r on 20.01.2017.
@@ -10,9 +25,49 @@ import java.io.File;
 
 public class RegistrationModel implements RegistrationContract.RegistrationModel {
 
-    String currentPhotoPath;
-    Uri userPicFileUri;
-    File userPicFile;
+    private final String TAG = "RegistrationModel";
+    private static final String PHONE_MASK = "+38 (0__) ___-__-__";
 
+    public String currentPhotoPath;
+    public Uri userPicFileUri;
+    public File userPicFile;
+    FormatWatcher formatWatcher;
 
+    @Override
+    public void getAllPermissionsToUserPicFile(Context context, Intent intent) {
+        List<ResolveInfo> resInfoList = context.getPackageManager().queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
+        for (ResolveInfo resolveInfo : resInfoList) {
+            String packageName = resolveInfo.activityInfo.packageName;
+            context.grantUriPermission(packageName, userPicFileUri, Intent.FLAG_GRANT_WRITE_URI_PERMISSION | Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        }
+    }
+
+    @Override
+    public void addPicToGallery(Context context) {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        userPicFile = new File(currentPhotoPath);
+        Uri contentUri = Uri.fromFile(userPicFile);
+        mediaScanIntent.setData(contentUri);
+        context.sendBroadcast(mediaScanIntent);
+    }
+
+    @Override
+    public File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File imageFile = File.createTempFile(imageFileName, ".jpg", storageDir);
+        currentPhotoPath = imageFile.getAbsolutePath();
+        userPicFile = new File(currentPhotoPath);
+        return userPicFile;
+    }
+
+    @Override
+    public FormatWatcher getFormatWatcher() {
+        Slot[] slots = new UnderscoreDigitSlotsParser().parseSlots(PHONE_MASK);
+        formatWatcher = new MaskFormatWatcher(
+                MaskImpl.createTerminated(slots)
+        );
+        return formatWatcher;
+    }
 }
--- app/src/main/java/com/internship/droidz/talkin/mvp/registration/RegistrationScreen.java
+++ app/src/main/java/com/internship/droidz/talkin/mvp/registration/RegistrationScreen.java
@@ -1,11 +1,9 @@
 package com.internship.droidz.talkin.mvp.registration;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -16,6 +14,7 @@ import android.widget.Toast;
 import com.internship.droidz.talkin.R;
 
 import ru.tinkoff.decoro.watchers.FormatWatcher;
+
 
 public class RegistrationScreen extends AppCompatActivity implements RegistrationContract.RegistrationView {
 
@@ -40,25 +39,8 @@ public class RegistrationScreen extends AppCompatActivity implements Registratio
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
 
-        presenter = new RegistrationPresenter(new RegistrationModel(), this);
+        presenter = new RegistrationPresenter(new RegistrationModel(), this, this);
         presenter.setFormatWatcher();
-    }
-
-    public void takePictureFromSource(View view) {
-        CharSequence sourcesOfImage[] = new CharSequence[] {"Device Camera", "Photo Gallery"};
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Choose source:")
-                 .setNegativeButton("BACK", (DialogInterface dialog, int which) -> {})
-                 .setItems(sourcesOfImage, (DialogInterface dialog, int which) -> {
-                    if (which == 0) {
-                        startActivityForResult(presenter.getCameraPictureIntent(getPackageManager(), this), REQUEST_IMAGE_CAPTURE);
-                    }
-                    if (which == 1) {
-                        Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                        startActivityForResult(galleryIntent, REQUEST_IMAGE_EXT);
-                    }
-                 })
-                .show();
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent returnedData) {
@@ -66,13 +48,11 @@ public class RegistrationScreen extends AppCompatActivity implements Registratio
         if (resultCode == RESULT_OK) {
             switch (requestCode) {
                 case REQUEST_IMAGE_CAPTURE : {//get image from camera
-                    presenter.checkImageAndSetToView();
-                    presenter.addPicToGallery(this);
+                    presenter.setupUserPicFromCamera();
                     break;
                 }
                 case REQUEST_IMAGE_EXT : { //if get from gallery
-                    presenter.setUserPicUri(returnedData.getData());
-                    presenter.checkImageAndSetToView();
+                    presenter.setupUserPicFromGallery(returnedData);
                     break;
                 }
             }
@@ -88,6 +68,17 @@ public class RegistrationScreen extends AppCompatActivity implements Registratio
     }
 
     @Override
+    public void startCameraForCapture() {
+        startActivityForResult(presenter.getCameraPictureIntent(getPackageManager()), REQUEST_IMAGE_CAPTURE);
+    }
+
+    @Override
+    public void startGalleryForCapture() {
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(galleryIntent, REQUEST_IMAGE_EXT);
+    }
+
+    @Override
     public void showAlertMaxSizeOfImage() {
         Toast.makeText(this, "File is bigger than 1MB", Toast.LENGTH_LONG).show();
     }
@@ -95,6 +86,10 @@ public class RegistrationScreen extends AppCompatActivity implements Registratio
     @Override
     public void showAlertFailedToLoad() {
         Toast.makeText(this, "Failed to load", Toast.LENGTH_SHORT).show();
+    }
+
+    public void userPicOnClick(View view) {
+        presenter.showDialogChooseSource();
     }
 
 
