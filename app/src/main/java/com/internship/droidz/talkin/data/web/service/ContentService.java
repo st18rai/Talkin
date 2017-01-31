4e6e6529d88c4feae9e50189725674552b6fcc6d
Alexandr Novak <joykerdp@gmail.com>
2017-01-31 11:07:30 +0200

    bloc upload confirm

--- app/src/main/java/com/internship/droidz/talkin/data/web/service/ContentService.java
+++ app/src/main/java/com/internship/droidz/talkin/data/web/service/ContentService.java
@@ -1,6 +1,7 @@
 package com.internship.droidz.talkin.data.web.service;
 
 import com.internship.droidz.talkin.data.web.requests.file.CreateFileRequest;
+import com.internship.droidz.talkin.data.web.requests.file.FileConfirmUploadRequest;
 import com.internship.droidz.talkin.data.web.response.file.CreateFileResponse;
 import com.internship.droidz.talkin.data.web.response.file.UploadFileResponse;
 
@@ -8,13 +9,16 @@ import java.util.Map;
 
 import okhttp3.MultipartBody;
 import okhttp3.RequestBody;
+import retrofit2.Response;
 import retrofit2.http.Body;
 import retrofit2.http.Header;
 import retrofit2.http.Headers;
 import retrofit2.http.Multipart;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
 import retrofit2.http.Part;
 import retrofit2.http.PartMap;
+import retrofit2.http.Path;
 import retrofit2.http.Url;
 import rx.Observable;
 
@@ -24,7 +28,7 @@ import rx.Observable;
 
 public interface ContentService {
 
-    @Headers({"Content-Type: application/json"})
+    @Headers("Content-Type: application/json")
     @POST("/blobs.json")
     Observable<CreateFileResponse> createFile(@Body CreateFileRequest request, @Header("QB-Token") String token);
 
@@ -32,4 +36,10 @@ public interface ContentService {
     @Multipart
     @POST
     Observable<UploadFileResponse> uploadFile(@Url String url, @PartMap Map<String, RequestBody> params, @Part MultipartBody.Part file);
+
+    @Headers("Content-Type: application/json")
+    @PUT("/blobs/{blob_id}/complete.json")
+    Observable<Response<Void>> fileConfirmUpload(@Path(value = "blob_id") String blobId,
+                                                  @Header("QB-Token") String token,
+                                                   @Body FileConfirmUploadRequest request);
 }
